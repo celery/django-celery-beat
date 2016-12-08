d798843b4d3505e35ed0330e4216175b99d56c65
Mark Heppner <mheppner@users.noreply.github.com>
2016-12-07 19:41:12 -0500

    Fix extra characters

--- django_celery_beat/migrations/0002_auto_20161118_0346.py
+++ django_celery_beat/migrations/0002_auto_20161118_0346.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(
                     auto_created=True, primary_key=True,
-                    serialize=False, absverbose_name='ID')),
+                    serialize=False, verbose_name='ID')),
                 ('event', models.CharField(
                     choices=[('dusk_nautical', 'dusk_nautical'),
                              ('dawn_astronomical', 'dawn_astronomical'),
