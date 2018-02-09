9e5584ed93792dd7a35796f9e8ad27afe57c4208
Patrick Paul <patrick@astrohaus.com>
2018-02-09 13:45:45 -0500

    Improvement: Adds field PeriodicTask.origin_key to track and remove tasks that are deleted from settings.beat_schedule in source code
    Bugfix: changing schedule class in settings.CELERYBEAT_SCHEDULE will no longer create a second (or third) schedule in PeriodicTask instance

--- django_celery_beat/models.py
+++ django_celery_beat/models.py
@@ -267,6 +267,9 @@ class PeriodicTask(models.Model):
     routing_key = models.CharField(
         _('routing key'), max_length=200, blank=True, null=True, default=None,
     )
+    origin_key = models.CharField(
+        _('origin key'), max_length=200, blank=True, null=True, default=None,
+    )
     expires = models.DateTimeField(
         _('expires'), blank=True, null=True,
     )
