12015e9cbdcefa81a09003568aed4a4cdadb16b7
Bachynin Ivan <bachynin.i@gmail.com>
2018-07-15 00:49:04 +0300

    add crontab validators

---
+++ django_celery_beat/validators.py
@@ -0,0 +1,44 @@
+import io
+from contextlib import redirect_stderr
+
+from crontab import CronTab
+from django.core.exceptions import ValidationError
+
+
+def validate_crontab(value, index):
+    """
+    Validate single part of cron tab.
+    :param value:
+    :param index: position in crontab
+    :return:
+    """
+    tab = ['*'] * 5
+    tab.append('dummy_command')  # force crontab parsing
+    tab[index] = value
+    tab = ' '.join(tab)
+    f = io.StringIO()
+    with redirect_stderr(f):
+        CronTab(tab=tab)
+    err = f.getvalue()
+    if err:
+        raise ValidationError(err)
+
+
+def minute_validator(value):
+    validate_crontab(value, 0)
+
+
+def hour_validator(value):
+    validate_crontab(value, 1)
+
+
+def day_of_month_validator(value):
+    validate_crontab(value, 2)
+
+
+def month_of_year_validator(value):
+    validate_crontab(value, 3)
+
+
+def day_of_week_validator(value):
+    validate_crontab(value, 4)
