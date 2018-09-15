2f2f78809bb0f9799ea56fb9f8f0d661825a73b2
Bachynin Ivan <16745500+vanyakosmos@users.noreply.github.com>
2018-09-15 08:05:13 +0300

    Add fields validation for CrontabSchedule (#154)
    * ignore pyenv .python-version file
    
    * add crontab validators
    
    * add validators unittests
    
    * py2.7 compatibility
    
    * add python-crontab to default requirements
    
    * use latest version of python-crontab
    
    in 2.3+ all bugs were fixed but support for py3.3-3.4 was droped (judging by setup.py tags)
    
    * 📝 rewrite crontab validation
    
    * 📚 add validators reference file to docs
    
    * 📚 include validators reference into index toctree
    
    * 📝 add migration
    
    * please linters

--- django_celery_beat/models.py
+++ django_celery_beat/models.py
@@ -3,19 +3,18 @@ from __future__ import absolute_import, unicode_literals
 
 from datetime import timedelta
 
+import timezone_field
+from celery import schedules
+from celery.five import python_2_unicode_compatible
 from django.core.exceptions import MultipleObjectsReturned, ValidationError
 from django.db import models
 from django.db.models import signals
 from django.utils.translation import ugettext_lazy as _
 
-from celery import schedules
-from celery.five import python_2_unicode_compatible
-
-from . import managers
+from . import managers, validators
 from .tzcrontab import TzAwareCrontab
-from .utils import now, make_aware
+from .utils import make_aware, now
 
-import timezone_field
 
 DAYS = 'days'
 HOURS = 'hours'
@@ -152,16 +151,25 @@ class CrontabSchedule(models.Model):
     # 4 chars for each value (what we save on 0-9 accomodates the []).
     # We leave the other fields at their historical length.
     #
-    minute = models.CharField(_('minute'), max_length=60 * 4, default='*')
-    hour = models.CharField(_('hour'), max_length=24 * 4, default='*')
+    minute = models.CharField(
+        _('minute'), max_length=60 * 4, default='*',
+        validators=[validators.minute_validator],
+    )
+    hour = models.CharField(
+        _('hour'), max_length=24 * 4, default='*',
+        validators=[validators.hour_validator],
+    )
     day_of_week = models.CharField(
         _('day of week'), max_length=64, default='*',
+        validators=[validators.day_of_week_validator],
     )
     day_of_month = models.CharField(
         _('day of month'), max_length=31 * 4, default='*',
+        validators=[validators.day_of_month_validator],
     )
     month_of_year = models.CharField(
         _('month of year'), max_length=64, default='*',
+        validators=[validators.month_of_year_validator],
     )
 
     timezone = timezone_field.TimeZoneField(default='UTC')
---
+++ docs/reference/django-celery-beat.validators.rst
@@ -0,0 +1,11 @@
+=====================================================
+ ``django_celery_beat.validators``
+=====================================================
+
+.. contents::
+    :local:
+.. currentmodule:: django_celery_beat.validators
+
+.. automodule:: django_celery_beat.validators
+    :members:
+    :undoc-members:
