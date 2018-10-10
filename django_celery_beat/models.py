f6b08a507db0d5445889f957fa68865cbb66d033
lucasdavila86 <lucasdavila86@users.noreply.github.com>
2018-10-10 17:37:40 -0300

    Update models.py

--- django_celery_beat/models.py
+++ django_celery_beat/models.py
@@ -325,7 +325,7 @@ class PeriodicTask(models.Model):
             raise ValidationError({
                 'crontab': [err_msg]
             })
-        if self.interval or self.solar:
+        if self.interval and self.solar:
             raise ValidationError({
                 'solar': [err_msg]
             })
