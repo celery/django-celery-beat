e8ac73a466b64b46f6c8e45668058a75b7665afe
Felix <tinylambda@gmail.com>
2018-10-11 21:05:20 +0800

    No transaction need here. and Avoid catching exceptions inside atomic! refer to: https://docs.djangoproject.com/en/2.1/topics/db/transactions/#controlling-transactions-explicitly
    optimization: only retry for the failed updates

--- django_celery_beat/schedulers.py
+++ django_celery_beat/schedulers.py
@@ -252,20 +252,20 @@ class DatabaseScheduler(Scheduler):
     def sync(self):
         info('Writing entries...')
         _tried = set()
+        _failed = set()
         try:
             close_old_connections()
 
             while self._dirty:
-                # with transaction.atomic():
                 name = self._dirty.pop()
-                _tried.add(name)
                 try:
                     self.schedule[name].save()
+                    _tried.add(name)
                 except (KeyError, ObjectDoesNotExist) as exc:
-                    pass
+                    _failed.add(name)
         except (DatabaseError, InterfaceError) as exc:
-            # retry later
-            self._dirty |= _tried
+            # retry later, only for the failed ones
+            self._dirty |= _failed
             logger.exception('Database error while sync: %r', exc)
 
     def update_from_dict(self, mapping):
