e177417490e141a0437fd285eb3cf8b0543bf222
Roman Sichnyi <roman.sichnyi@globallogic.com>
2018-10-12 22:12:28 +0300

    change test name to fix line length

--- t/unit/test_schedulers.py
+++ t/unit/test_schedulers.py
@@ -406,7 +406,7 @@ class test_DatabaseScheduler(SchedulerCase):
         monkeypatch.setattr(self.s, 'schedule_changed', lambda: True)
         self.s.tick()
 
-    def test_update_scheduler_heap_invalidation_not_to_break_heap(self, monkeypatch):
+    def test_heap_size_is_constant(self, monkeypatch):
         # heap size is constant unless the schedule changes
         monkeypatch.setattr(self.s, 'schedule_changed', lambda: True)
         expected_heap_size = len(self.s.schedule.values())
