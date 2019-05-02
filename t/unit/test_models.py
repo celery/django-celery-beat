6cdc98c5a123a961b7ff4b61a7b2c5fe3cc69d99
Sebastian S <moseb@users.noreply.github.com>
2019-05-02 18:55:21 +0200

    migrations: Teach MigrationTests about existing duplicate migration numbers

--- t/unit/test_models.py
+++ t/unit/test_models.py
@@ -12,7 +12,7 @@ from django_celery_beat import migrations as beat_migrations
 
 
 class MigrationTests(TestCase):
-    def test_no_duplicate_migration_numbers(self):
+    def test_no_future_duplicate_migration_numbers(self):
         """Verify no duplicate migration numbers.
 
         Migration files with the same number can cause issues with
@@ -20,9 +20,15 @@ class MigrationTests(TestCase):
         """
         path = os.path.dirname(beat_migrations.__file__)
         files = [f[:4] for f in os.listdir(path) if f.endswith('.py')]
+        expected_duplicates = [
+            (3, '0006'),
+        ]
+        duplicates_extra = sum(count - 1 for count, _ in expected_duplicates)
+        duplicates_numbers = [number for _, number in expected_duplicates]
         self.assertEqual(
-            len(files), len(set(files)),
-            msg='Detected migration files with the same migration number')
+            len(files), len(set(files)) + duplicates_extra,
+            msg=('Detected migration files with the same migration number'
+                 ' (besides {})'.format(' and '.join(duplicates_numbers))))
 
     def test_models_match_migrations(self):
         """Make sure that no model changes exist.
