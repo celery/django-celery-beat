7d6a67a091fbf72c52f916ac60ee0210141f64e4
Michał Bielawski <michal.bielawski@apperio.com>
2020-01-13 18:41:50 +0000

    Drop support for Django <2.2 and Python 2.

--- setup.py
+++ setup.py
@@ -40,19 +40,16 @@ classes = """
     Development Status :: 5 - Production/Stable
     License :: OSI Approved :: BSD License
     Programming Language :: Python
-    Programming Language :: Python :: 2
-    Programming Language :: Python :: 2.7
     Programming Language :: Python :: 3
-    Programming Language :: Python :: 3.6
     Programming Language :: Python :: 3.5
+    Programming Language :: Python :: 3.6
     Programming Language :: Python :: 3.7
+    Programming Language :: Python :: 3.8
     Programming Language :: Python :: Implementation :: CPython
     Programming Language :: Python :: Implementation :: PyPy
     Framework :: Django
-    Framework :: Django :: 2.1
     Framework :: Django :: 2.2
-    Framework :: Django :: 1.11
-    Framework :: Django :: 2.0
+    Framework :: Django :: 3.0
     Operating System :: OS Independent
     Topic :: Communications
     Topic :: System :: Distributed Computing
