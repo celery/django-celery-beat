a979d0a6a32e56e116a3fddaacd0d03ea79d0960
Asif Saif Uddin (Auvi) <auvipy@gmail.com>
2021-07-02 11:12:27 +0600

    Bump version: v2.2.0 → v2.2.1

--- django_celery_beat/__init__.py
+++ django_celery_beat/__init__.py
@@ -8,7 +8,7 @@ from collections import namedtuple
 
 import django
 
-__version__ = '2.2.0'
+__version__ = '2.2.1'
 __author__ = 'Asif Saif Uddin, Ask Solem'
 __contact__ = 'auvipy@gmail.com, ask@celeryproject.org'
 __homepage__ = 'https://github.com/celery/django-celery-beat'
