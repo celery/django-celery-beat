49d9331322f4c386fe33965e2d9ee566d1ec7575
Diego Castro <diego.castro@kubicum.com>
2022-04-11 10:35:52 +0200

    Support for abstract base models improved. (#516)
    Model classes moved into a `models.py` directory, which exports by
    default only the default `django-celery-beat` model classes now from
    `models.generic.py`.
    
    `managers.py` moved into `models.py` directory, because it's something
    that is needed only by models.

---
+++ django_celery_beat/models/__init__.py
@@ -0,0 +1,19 @@
+from .generic import (
+    ClockedSchedule,
+    ClockScheduler,
+    CrontabSchedule,
+    IntervalSchedule,
+    PeriodicTask,
+    PeriodicTasks,
+    SolarSchedule
+)
+
+__ALL__ = [
+    "ClockedSchedule",
+    "ClockScheduler",
+    "CrontabSchedule",
+    "IntervalSchedule",
+    "PeriodicTask",
+    "PeriodicTasks",
+    "SolarSchedule"
+]
