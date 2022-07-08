e77da44a029acafefe58e45844718f31458286dd
Jens Nistler <opensource@jensnistler.de>
2022-07-08 17:55:19 +0200

    fix line length

--- django_celery_beat/querysets.py
+++ django_celery_beat/querysets.py
@@ -3,7 +3,10 @@ from django.db import models
 
 
 class PeriodicTaskQuerySet(models.QuerySet):
-    """QuerySet for the PeriodicTask model that will be used as it's default manager."""
+    """
+    QuerySet for the PeriodicTask model that
+    will be used as it's default manager.
+    """
 
     def enabled(self):
         return self.filter(enabled=True)
