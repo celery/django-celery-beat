b3152a8993c00fc88b822eea07218bf5a19654ab
Jens Nistler <opensource@jensnistler.de>
2022-07-11 09:50:52 +0200

    fix docstring

--- django_celery_beat/querysets.py
+++ django_celery_beat/querysets.py
@@ -3,10 +3,7 @@ from django.db import models
 
 
 class PeriodicTaskQuerySet(models.QuerySet):
-    """
-    QuerySet for the PeriodicTask model that
-    will be used as it's default manager.
-    """
+    """QuerySet for PeriodicTask."""
 
     def enabled(self):
         return self.filter(enabled=True)
