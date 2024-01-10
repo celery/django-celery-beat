35a3f5d21593908e2094c51fa4fecba1099d1af4
Christian Clauss <cclauss@me.com>
2024-01-10 08:01:24 +0100

    Prepare for release v2.6.0 to support Py3.12 and Dj5.0

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ classes = """
     Framework :: Django :: 3.2
     Framework :: Django :: 4.1
     Framework :: Django :: 4.2
+    Framework :: Django :: 5.0
     Operating System :: OS Independent
     Topic :: Communications
     Topic :: System :: Distributed Computing
