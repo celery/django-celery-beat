12e516660623727a546d6ec6c555ae01f084bd00
arthru <arthur@hashbang.fr>
2024-06-26 17:28:11 +0200

    some fixes in fr translation (#760)
    * some fixes in fr translation
    
    * compile fr translations
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * don't extract the verb from the translation string
    
    * fix plural
    
    * remove a fuzzy and fix the PO creation date
    
    * compilemessages fr
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- django_celery_beat/admin.py
+++ django_celery_beat/admin.py
@@ -8,6 +8,7 @@ from django.db.models import Case, Value, When
 from django.forms.widgets import Select
 from django.template.defaultfilters import pluralize
 from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ngettext_lazy
 from kombu.utils.json import loads
 
 from .models import (ClockedSchedule, CrontabSchedule, IntervalSchedule,
@@ -169,29 +170,20 @@ class PeriodicTaskAdmin(admin.ModelAdmin):
         qs = super().get_queryset(request)
         return qs.select_related('interval', 'crontab', 'solar', 'clocked')
 
-    def _message_user_about_update(self, request, rows_updated, verb):
-        """Send message about action to user.
-
-        `verb` should shortly describe what have changed (e.g. 'enabled').
-
-        """
-        self.message_user(
-            request,
-            _('{0} task{1} {2} successfully {3}').format(
-                rows_updated,
-                pluralize(rows_updated),
-                pluralize(rows_updated, _('was,were')),
-                verb,
-            ),
-        )
-
     @admin.action(
         description=_('Enable selected tasks')
     )
     def enable_tasks(self, request, queryset):
         rows_updated = queryset.update(enabled=True)
         PeriodicTasks.update_changed()
-        self._message_user_about_update(request, rows_updated, 'enabled')
+        self.message_user(
+            request,
+            ngettext_lazy(
+                '{0} task was successfully enabled',
+                '{0} tasks were successfully enabled',
+                rows_updated
+            ).format(rows_updated)
+        )
 
     @admin.action(
         description=_('Disable selected tasks')
@@ -199,7 +191,14 @@ class PeriodicTaskAdmin(admin.ModelAdmin):
     def disable_tasks(self, request, queryset):
         rows_updated = queryset.update(enabled=False, last_run_at=None)
         PeriodicTasks.update_changed()
-        self._message_user_about_update(request, rows_updated, 'disabled')
+        self.message_user(
+            request,
+            ngettext_lazy(
+                '{0} task was successfully disabled',
+                '{0} tasks were successfully disabled',
+                rows_updated
+            ).format(rows_updated)
+        )
 
     def _toggle_tasks_activity(self, queryset):
         return queryset.update(enabled=Case(
@@ -213,7 +212,14 @@ class PeriodicTaskAdmin(admin.ModelAdmin):
     def toggle_tasks(self, request, queryset):
         rows_updated = self._toggle_tasks_activity(queryset)
         PeriodicTasks.update_changed()
-        self._message_user_about_update(request, rows_updated, 'toggled')
+        self.message_user(
+            request,
+            ngettext_lazy(
+                '{0} task was successfully toggled',
+                '{0} tasks were successfully toggled',
+                rows_updated
+            ).format(rows_updated)
+        )
 
     @admin.action(
         description=_('Run selected tasks')
