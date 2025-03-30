c454c65d27278100ad826680339548f61e440591
JinRiYao2001 <jinriyao@qq.com>
2025-03-30 15:02:40 +0800

    Fix the time calculation problem caused by start_time (#844)
    * Fix the time calculation problem caused by start_time
    
    * Code Formatting
    
    * Modify the delay generation logic
    
    * created a method to calculate the exact delay time
    
    * Fix syntax errors
    
    * Changed due_start_time calculation logic and added unit test
    
    * Code formatting
    
    * formatting
    
    * Fixed time zone issue and added a test case
    
    * use zoneinfo
    
    * Force the time zone of start_time to be converted to tz
    
    * change test requirements
    
    * rollback requirements/test.txt
    
    * Update t/unit/test_schedulers.py
    
    * Adaptive py3.8
    
    * Update t/unit/test_schedulers.py
    
    * Update django_celery_beat/schedulers.py
    
    * Update django_celery_beat/schedulers.py
    
    * Change the parameter name to solve the problem of too long code and solve the problem of test.txt dependency
    
    ---------
    
    Co-authored-by: Asif Saif Uddin <auvipy@gmail.com>

--- t/unit/test_schedulers.py
+++ t/unit/test_schedulers.py
@@ -5,6 +5,11 @@ from datetime import datetime, timedelta
 from itertools import count
 from time import monotonic
 
+try:
+    from zoneinfo import ZoneInfo  # Python 3.9+
+except ImportError:
+    from backports.zoneinfo import ZoneInfo  # Python 3.8
+
 import pytest
 from celery.schedules import crontab, schedule, solar
 from django.contrib.admin.sites import AdminSite
@@ -775,6 +780,123 @@ class test_DatabaseScheduler(SchedulerCase):
         s.tick()
         assert s._heap[0]
         assert s._heap[0][2].name == m1.name
+
+    def test_crontab_with_start_time_between_now_and_crontab(self, app):
+        now = app.now()
+        delay_minutes = 2
+
+        test_start_time = now + timedelta(minutes=delay_minutes)
+
+        crontab_time = test_start_time + timedelta(minutes=delay_minutes)
+
+        task = self.create_model_crontab(
+            crontab(minute=f'{crontab_time.minute}'),
+            start_time=test_start_time)
+
+        entry = EntryTrackSave(task, app=app)
+
+        is_due, next_check = entry.is_due()
+
+        expected_delay = 2 * delay_minutes * 60
+
+        assert not is_due
+        assert next_check == pytest.approx(expected_delay, abs=60)
+
+    def test_crontab_with_start_time_after_crontab(self, app):
+        now = app.now()
+
+        delay_minutes = 2
+
+        crontab_time = now + timedelta(minutes=delay_minutes)
+
+        test_start_time = crontab_time + timedelta(minutes=delay_minutes)
+
+        task = self.create_model_crontab(
+            crontab(minute=f'{crontab_time.minute}'),
+            start_time=test_start_time)
+
+        entry = EntryTrackSave(task, app=app)
+
+        is_due, next_check = entry.is_due()
+
+        expected_delay = delay_minutes * 60 + 3600
+
+        assert not is_due
+        assert next_check == pytest.approx(expected_delay, abs=60)
+
+    def test_crontab_with_start_time_different_time_zone(self, app):
+        now = app.now()
+
+        delay_minutes = 2
+
+        test_start_time = now + timedelta(minutes=delay_minutes)
+
+        crontab_time = test_start_time + timedelta(minutes=delay_minutes)
+
+        tz = ZoneInfo('Asia/Shanghai')
+        test_start_time = test_start_time.astimezone(tz)
+
+        task = self.create_model_crontab(
+            crontab(minute=f'{crontab_time.minute}'),
+            start_time=test_start_time)
+
+        entry = EntryTrackSave(task, app=app)
+
+        is_due, next_check = entry.is_due()
+
+        expected_delay = 2 * delay_minutes * 60
+
+        assert not is_due
+        assert next_check == pytest.approx(expected_delay, abs=60)
+
+        now = app.now()
+
+        crontab_time = now + timedelta(minutes=delay_minutes)
+
+        test_start_time = crontab_time + timedelta(minutes=delay_minutes)
+
+        tz = ZoneInfo('Asia/Shanghai')
+        test_start_time = test_start_time.astimezone(tz)
+
+        task = self.create_model_crontab(
+            crontab(minute=f'{crontab_time.minute}'),
+            start_time=test_start_time)
+
+        entry = EntryTrackSave(task, app=app)
+
+        is_due, next_check = entry.is_due()
+
+        expected_delay = delay_minutes * 60 + 3600
+
+        assert not is_due
+        assert next_check == pytest.approx(expected_delay, abs=60)
+
+    def test_crontab_with_start_time_tick(self, app):
+        PeriodicTask.objects.all().delete()
+        s = self.Scheduler(app=self.app)
+        assert not s._heap
+
+        m1 = self.create_model_interval(schedule(timedelta(seconds=3)))
+        m1.save()
+
+        now = timezone.now()
+        start_time = now + timedelta(minutes=1)
+        crontab_trigger_time = now + timedelta(minutes=2)
+
+        m2 = self.create_model_crontab(
+            crontab(minute=f'{crontab_trigger_time.minute}'),
+            start_time=start_time)
+        m2.save()
+
+        e2 = EntryTrackSave(m2, app=self.app)
+        is_due, _ = e2.is_due()
+
+        max_iterations = 1000
+        iterations = 0
+        while (not is_due and iterations < max_iterations):
+            s.tick()
+            assert s._heap[0][2].name != m2.name
+            is_due, _ = e2.is_due()
 
 
 @pytest.mark.django_db
