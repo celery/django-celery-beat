5403b09a65c04ef8df8c108c709894f4fbcfb1dd
Steven <rh0dium@users.noreply.github.com>
2025-08-31 12:30:43 -0700

    Update django_celery_beat/models.py
    Add comment per request
    
    Co-authored-by: Christian Clauss <cclauss@me.com>

--- django_celery_beat/models.py
+++ django_celery_beat/models.py
@@ -12,7 +12,7 @@ from celery import current_app, schedules
 try:
     from cron_descriptor import (FormatError, MissingFieldError,
                                  WrongArgumentError, get_description)
-except ImportError:
+except ImportError:  # Python < 3.11 must use cron_descriptor < 2.0 which uses *Exception exceptions.
     from cron_descriptor import FormatException as FormatError
     from cron_descriptor import MissingFieldException as MissingFieldError
     from cron_descriptor import WrongArgumentException as WrongArgumentError
