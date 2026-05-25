ca30c95a057e0a80171c24ea5bce24891fdb8535
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2026-05-25 10:48:03 +0200

    Confirm Django 6.1 support

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ classes = """
     Framework :: Django :: 5.1
     Framework :: Django :: 5.2
     Framework :: Django :: 6.0
+    Framework :: Django :: 6.1
     Operating System :: OS Independent
     Topic :: Communications
     Topic :: System :: Distributed Computing
